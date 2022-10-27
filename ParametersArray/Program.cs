b10bd2439da59ab6817562398a12878ca34fab1c
Andrew Midthun <midth002@gmail.com>
2022-10-27 18:25:59 -0500

    Initial commit. Array checklist in c# for student grades.

---
+++ ParametersArray/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace ParametersArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] studentsGrades = new int[] { 88, 91, 76, 64, 60, 99, 74 };
+           Console.WriteLine(GetAverage(studentsGrades));
+
+            foreach(int student in studentsGrades)
+            {
+                if(student > 89)
+                {
+                    Console.WriteLine("A");
+                } 
+                else if (student > 79)
+                {
+                    Console.WriteLine("B");
+                }
+                else if (student > 69)
+                {
+                    Console.WriteLine("C");
+                }
+                else if (student > 59)
+                {
+                    Console.WriteLine("D");
+                } 
+                else
+                {
+                    Console.WriteLine("F"); 
+                }
+                
+            }
+        }
+
+        static double GetAverage(int[] gradesArray)
+        {
+            int size = gradesArray.Length;
+            double average;
+            int sum = 0;
+
+            for(int i = 0; i < size; i++)
+            {
+                sum += gradesArray[i];
+            }
+
+            average = (double) sum / size;
+
+            return average;
+        }
+    }
+}
